299a1bdad923da192ab3c05e10ce0bf9e3c0867a
janki-amrutiya <amrutiya_janki@network.lilly.com>
2018-04-09 17:03:08 +0530

    Added jquery ajax

--- src/views/NewsDashboard/NewsDashboard.jsx
+++ src/views/NewsDashboard/NewsDashboard.jsx
@@ -1,8 +1,9 @@
+/* global $ */
+
 import React, { Component } from 'react';
 import Card from '../../components/Card/Card';
 import Header from '../../components/Header/Header';
 import Tabs from '../../components/Tabs/Tabs';
-
 const OPTIONS = [
   'new',
   'rising',
@@ -19,23 +20,41 @@ class NewsDashboard extends Component {
   }
 
   componentDidMount(){
-    fetch('https://mashable.com/stories.json?hot_per_page=2&new_per_page=20&rising_per_page=3&channel=tech&new_after&1Dvhfi')
-    .then(response => {
-      if (response.ok) {
-        return Promise.resolve(response);
-      } else {
-        return Promise.reject(new Error('Failed to load'));
-      }
+    $.get({
+      // The URL for the request
+      url: "https://mashable.com/stories.json",
+      // The data to send (will be converted to a query string)
+      data: {
+        hot_per_page:'2',
+        new_per_page:'20',
+        rising_per_page:'3',
+        channel:'tech',
+        new_after:'1Dvhfi'
+      },
+      type: "GET",
+      dataType : "json",
+  })
+    // Code to run if the request succeeds;
+    // The response is passed to the function
+    .done(( response ) => {
+       return response;
     })
-    .then(response => response.json()) // parse response as JSON
-    .then(data => {
+    .done(data=>{
       this.setState({
         data
       })
     })
+    // Code to run if the request fails
+    // status codes are passed to the function
+    .fail(function( xhr, status, errorThrown ) {
+     // alert( "Sorry, there was a problem!" );
+      console.log( "Error: " + errorThrown );
+      console.log( "Status: " + status );
+    })
+    .always(function( xhr, status ) {
+      console.log("complete");
+    });
   }
-
-
 
   changedTab = (data) => {
     this.setState({
